45a7dc9e1f53623335db7fa85ecfc540ce649a36
Allison Hembd <ahembd9@gmail.com>
2016-02-23 19:15:48 -0600

    preparing to publish

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,28 +1,82 @@
 $(document).ready(function(){
-    var products = [
-      {
-        name: "Amazon Fire TV",
-        manufacturer: "Amazoon",
-        imageSrc: "assets/images/product01.jpg",
-        description: "Eligible for free shipping and returns",
-        price: 89.99
-      },
-      {
-        name: "Amazon Fire",
-        manufacturer: "Amazoon",
-        imageSrc: "assets/images/product02.jpg",
-        description: "Eligible for free shipping and returns",
-        price: 69.00
-      },
-      {
-        name: "Amazon Echo",
-        manufacturer: "Amazoon",
-        imageSrc: "assets/images/product03.jpg",
-        description: "Eligible for free shipping and returns",
-        price: 109.99
-      }
-    ];
 
-    // Get started here!
+  $.get('https://www.batchacademy.com/api/wdfne/dummy/amazoon-products', function(products){
+    //IN HERE WE HAVE ACCESS TO THE DATA FROM THE SERVER
+
+    var updateView = function(){
+      var cart = $('.cart .product');
+      var saved = $('.saved .product');
+
+      if(cart.length === 0){
+        $('.cart .empty').show();
+        $('.cart-total').hide();
+      } else {
+        $('.cart .empty').hide();
+        $('.cart-total').show();
+      }
+
+      if(saved.length === 0){
+        $('.saved .empty').show();
+      } else {
+        $('.saved .empty').hide();
+      }
+
+      var cartTotal = 0;
+      $(cart).each(function(){
+        cartTotal += Number($(this).data('price'));
+      });
+
+      $('.cart-total span').text('$' + cartTotal.toFixed(2));
+
+      updateView();
+
+    }
+    var productTemplate = $('template#product').html();
+
+      for(var i = 0; i < products.length; i++){
+        var newProduct = $(productTemplate);
+
+        $('h2', newProduct).text(products[i].name);
+        var newManufacturer = $('<span/>').addClass('manufacturer').text('by ' + products[i].manufacturer);
+        $('h2', newProduct).append(newManufacturer);
+        $('.image img', newProduct).attr('src', products[i].imageSrc);
+        $('h3', newProduct).text(products[i].description);
+        $('.price, .mobile-price', newProduct).text('$' + products[i].price.toFixed(2));
+        $(newProduct).data('price', products[i].price);
+
+        $('.cart').append(newProduct);
+      }
+
+      $('.move').on('click', function(){
+        if ($(this).parents().eq(4).hasClass('cart')) {
+        $('.saved').append($(this).parents().eq(3));
+        $(this).text('Add to Cart');
+      } else {
+        $('.cart').append($(this).parents().eq(3));
+        $(this).text('Save for Later');
+      }
+
+      updateView();
+
+      });
+
+      // function updateView() {
+      //   var products = $('.products .product');
+      //   var total = 0;
+      //   $(products).each(function(){
+      //   total += Number($(this).data('price'));
+      // }
+      // });
+      //   console.log(total);
+
+  
 
   });
+  
+//OUT HERE WE DONT HAVE ACCESS TO THE DATA
+
+      
+     });
+  
+
+
--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
-  // Get started here!
+
+
+  $('form.order-form').on('submit', function(event){
+  event.preventDefault();
+      
+  var isValid = true;
+      
+  $('input[required]').each(function() {
+    if ( $(this).val().length > 0) {
+      $(this).parent().removeClass('has-error')
+      } else {
+      $(this).parent().addClass('has-error')
+      isValid = false;
+      }
+    });
+
+    if (isValid === false) {
+      $('.error-message').show();
+    } else {
+      $('.error-message').hide();
+      $('.has-error').removeClass;
+      alert('Success!');
+    }
+});
+
 });
